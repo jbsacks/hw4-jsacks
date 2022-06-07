271b8aa5d125671e1cd1fda660600f05a5360e86
jbsacks <josh.sacks@kellogg.northwestern.edu>
2022-06-07 22:53:35 +0000

    login and users and sessions halfway

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,23 @@ class SessionsController < ApplicationController
   def destroy
   end
 
-  if BCrypt::Password.new(@user["password"]) == params["password"]
+  # if BCrypt::Password.new(@user["password"]) == params["password"]
 
 end
-  
+
+def create
+  @user = User.find_by({ "email" => params["email"] })
+  if @user
+    if @user["password"] == params["password"]
+      # login the user
+      flash["notice"] = "You've logged in."
+      redirect_to "/posts"
+    else
+      flash["notice"] = "Unsuccessful login."
+      redirect_to "/sessions/new"
+    end
+  else
+    flash["notice"] = "Unsuccessful login."
+    redirect_to "/sessions/new"
+  end
+end
